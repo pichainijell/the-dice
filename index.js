7461d5fdced5b3e8f081827967b78ab789878b80
9jell <valid.nam@gmail.com>
2025-02-05 11:07:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function rollDice(){
+  // Dice number 1
   var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
   var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
@@ -9,7 +10,7 @@ function rollDice(){
 
   image1.setAttribute("src", randomImageSource);
 
-
+  // Dice number 2
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
   var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
@@ -21,9 +22,11 @@ function rollDice(){
   if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
   }
+  // If player 2 wins
   else if (randomNumber2 > randomNumber1) {
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
   }
+  // If Draw
   else {
     document.querySelector("h1").innerHTML = "Draw!";
   }
